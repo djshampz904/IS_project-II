b846922162b5f82c1f219a4d2d1a03647d6f0374
djshampz904 <djshampz05@gmail.com>
2024-02-02 18:54:16 +0300

    elt pipeline code

--- etl.py
+++ etl.py
@@ -5,7 +5,7 @@ import mysql.connector
 
 def extract_data_from_csv(file_path):
     """Extract data from csv file and return a dataframe"""
-    df = pd.read_csv(file_path, sep=';')
+    df = pd.read_csv(file_path, sep=',')
     return df
 
 def create_db_connection(host_name, user_name, user_password, db_name):
@@ -33,14 +33,21 @@ def extract_column_names(df):
 def create_column_types(df):
     """Create column types for a database"""
     column_types = []
-    for column in df.dtypes:
-        if column == 'object':
-            column_types.append('VARCHAR(255)')
-        elif column == 'int64':
-            column_types.append('INT')
-        elif column == 'float64':
+    for column, dtype in df.dtypes.items():
+        if dtype == 'float64':
             column_types.append('FLOAT')
-        elif column == 'datetime64[ns]':
+        elif dtype == 'object':
+            try:
+                # Try to convert the column to integers in the DataFrame
+                df[column] = df[column].astype(int)
+                # If successful, add 'INT' to the column types
+                column_types.append('INT')
+            except ValueError:
+                # If it fails, add 'VARCHAR(255)' to the column types
+                column_types.append('VARCHAR(255)')
+        elif dtype == 'int64':
+            column_types.append('INT')
+        elif dtype == 'datetime64[ns]':
             column_types.append('DATETIME')
     return column_types
 
@@ -53,16 +60,6 @@ def create_table(connection, query):
     except mysql.connector.Error as err:
         print(f"Error: '{err}'")
 
-def insert_data_into_table(connection, query, values):
-    """Insert data into a table"""
-    cursor = connection.cursor()
-    try:
-        cursor.executemany(query, values)
-        connection.commit()
-        print("Data inserted successfully")
-    except mysql.connector.Error as err:
-        print(f"Error: '{err}'")
-
 def drop_table(connection, query):
     """Drop a table"""
     cursor = connection.cursor()
@@ -72,11 +69,11 @@ def drop_table(connection, query):
     except mysql.connector.Error as err:
         print(f"Error: '{err}'")
 
-def insert_data_into_table(connection, query, values):
+def insert_data_into_table(connection, query):
     """Insert data into a table"""
     cursor = connection.cursor()
     try:
-        cursor.executemany(query, values)
+        cursor.execute(query)
         connection.commit()
         print("Data inserted successfully")
     except mysql.connector.Error as err:
--- main..py
+++ main..py
@@ -4,9 +4,12 @@ import pandas as pd
 if __name__ == "__main__":
     file_path = "data/WA_Fn-UseC_-Telco-Customer-Churn.csv"
     df = pd.read_csv(file_path, sep=',', header=0)
+    
+    df.columns = map(str.lower, df.columns)
+    df['totalcharges'] = pd.to_numeric(df['totalcharges'], errors='coerce')
     table_names = {
                 "customer_table": ["customerid", "gender", "seniorcitizen", "partner", "dependents"],
-                "service_table": ["customerid", "phonedervice", "multiplelines", "internetservice"],
+                "service_table": ["customerid", "phoneservice", "multiplelines", "internetservice"],
                 "security_table": ["customerid", "onlinesecurity", "onlinebackup", "deviceprotection", "techsupport"],
                 "streaming_table": ["customerid", "streamingtv", "streamingmovies"],
                 "billing_table": ["customerid", "contract", "paperlessbilling", "paymentmethod", "monthlycharges", "totalcharges", "churn"]
@@ -35,3 +38,23 @@ if __name__ == "__main__":
         query = query.rstrip(",")  # remove trailing comma
         query += " )"
         etl.create_table(connection, query)
+        
+        
+    #insert data into the database
+    for table, columns in table_names.items():
+        query = f"INSERT INTO {table} ("
+        for column in table_names[table]:
+            query += f"{column}, "
+        query = query.rstrip(", ")
+        query += ") VALUES ("
+        for row in df[columns].values:
+            for value in row:
+                if not isinstance(value, int):
+                    query += f"'{value}', "
+                else:
+                    query += f"{value}, "
+            query = query.rstrip(", ")
+            query += "), ("
+        query = query.rstrip(", (")
+        query += ";"
+        print(query)
